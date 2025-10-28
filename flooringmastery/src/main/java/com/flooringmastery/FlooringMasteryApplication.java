f49906bb7ea70005ca4f25193f2e93e38a6e6b1d
sasha-sokol <sasha.sokolniki@gmail.com>
2025-10-28 14:12:41 +0000

    Set up springboot annotations

--- flooringmastery/src/main/java/com/flooringmastery/FlooringMasteryApplication.java
+++ flooringmastery/src/main/java/com/flooringmastery/FlooringMasteryApplication.java
@@ -1,13 +1,20 @@
 package com.flooringmastery;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.flooringmastery.controller.FlooringMasteryController;
 
 @SpringBootApplication
 public class FlooringMasteryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FlooringMasteryApplication.class, args);
+		AnnotationConfigApplicationContext appContext = new AnnotationConfigApplicationContext();
+		appContext.scan("com.flooringmastery");
+		appContext.refresh();
+		
+		FlooringMasteryController controller = appContext.getBean("flooringMasteryController", FlooringMasteryController.class);
+		controller.run();
 	}
 
 }
--- flooringmastery/src/main/java/com/flooringmastery/controller/FlooringMasteryController.java
+++ flooringmastery/src/main/java/com/flooringmastery/controller/FlooringMasteryController.java
@@ -1,5 +1,8 @@
 package com.flooringmastery.controller;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class FlooringMasteryController {
     public void run() {
         
